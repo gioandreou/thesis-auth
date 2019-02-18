546ac9e997782bca404928873bbf68c6dbd53409
giorgos-andreou <gioandreou1995@gmail.com>
2019-02-18 15:06:32 +0200

    charts pies

--- running files/charts.py
+++ running files/charts.py
@@ -37,11 +37,39 @@ def plot_ages_gender_df(dataframe):
     print("Age-Men successfully ploted")
     plt.clf()
 
+def plot_city_country_locale(dataframe,typpe):
+    labels=list(dataframe.columns.values)
+    date=labels[0]
+
+    sub_df= dataframe.iloc[:,1:]
+    top10 = sub_df.mean().nlargest(10)
+   
+    print(top10)
+   
+    fig, ax = plt.subplots(figsize=(12, 12))
+    top10.plot(kind='bar',use_index=True,position=0.8, grid=True,fontsize=8,rot=6,)
+    #plt.ylabel('People that are talking about the Page',fontsize=15)
+    plt.title('Top 10 Cities with Fans of the Page',fontsize=12)
+    plt.ylabel('Chart',fontsize=12)
+    plt.savefig("charts_new/City-bar"+".png",dpi=300)
+    print("City bar-chart was created!")
+    plt.clf()
+
+    top10.plot(kind='pie',autopct='%1.1f%%',textprops=dict(color="black"))
+    plt.ylabel('People that are talking about the Page',fontsize=15)
+    plt.title('Top 10 Cities with Fans of the Page',fontsize=12)
+    plt.savefig("charts_new/City-pie"+".png",dpi=300)
+    print("City bar-pie was created!")
+    plt.clf()
 
 
 def main():
-
+    '''
     age_gender = pd.read_excel('excels/lite/lite-Ages-Gender.xlsx')
     plot_ages_gender_df(age_gender)
+    '''
+    city = pd.read_excel('excels/lite/lite-City.xlsx')
+    plot_city_country_locale(city,"City")
+
 
 main()
