eb1ab81ef1b88e9e941747dd1e7af0eaba5fb2b4
giorgos-andreou <gioandreou1995@gmail.com>
2019-02-25 18:14:23 +0200

    high-mid-low education

--- running files/elstat_correlation.py
+++ running files/elstat_correlation.py
@@ -9,18 +9,57 @@ import plotly.graph_objs as go
 import plotly.io as pio
 from tabulate import tabulate
 
+def striplist(l):
+    return([x.strip() for x in l])
 
-def print_dataframes_excels(dataframe):
+
+def print_dataframes_education(regions,dataframe):
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-            labels_list = list(dataframe.index)
-            rows = dataframe.loc[ [ (' Thessaly, Greece', 'Male'), ('Thessaly, Greece', 'Female') ] ,'Total'   ]
-            print(rows)
+        intial_region_list = list(regions.index)
+        regions_labels_list=striplist(intial_region_list)
+        
+        region_df = pd.DataFrame()
+        region_df['Total']=dataframe['Total']
+        
+        higher_edu = (dataframe['PhD']+dataframe['Masters']+dataframe['Bachelor']+dataframe['Technological Educational Institute'])
+        middle_edu = (dataframe['Upper Vocational Private Schools']+dataframe['Institute Vocational Training - IEK']+dataframe['High School']+dataframe['Vocational upper secondary schools']+dataframe['Vocational Private Schools'])
+        lower_edu = (dataframe['Middle School\n']+dataframe['Elementary School']+dataframe['Abandoned Elementary - Can Read Write']+dataframe['Illeterate'])
+
+        percen_higher = ((higher_edu/dataframe['Total'])*100).round(2)
+        percen_middle = ((middle_edu/dataframe['Total'])*100).round(2)
+        percen_lower = ((lower_edu/dataframe['Total'])*100).round(2)
 
 
+        region_df['Higher Education']= (higher_edu.map(str)+"--["+percen_higher.map(str)+ "%]")
+        region_df['Middle Education']= (middle_edu.map(str)+"--["+percen_middle.map(str)+ "%]")
+        region_df['Lower Education']= (lower_edu.map(str)+"--["+percen_lower.map(str)+ "%]")
+        region_df['Higher Education Percentage[%]']= (percen_higher)
+        region_df['Middle Education Percentage[%]']= (percen_middle)
+        region_df['Lower Education Percentage[%]']= (percen_lower)
+        
+        all_regions_label_list = ['Greece','Eastern Macedonia and Thrace, Greece','Central Macedonia, Greece','Western Macedonia, Greece','Epirus (region), Greece','Thessaly, Greece','Central Greece (region), Greece',
+                                'Ionian Islands (region), Greece','Western Greece, Greece','Peloponnese (region), Greece','Attica (region), Greece','Northern Aegean, Greece','Southern Aegean, Greece','Crete, Greece']
+        
+        print(intial_region_list)
+        print(regions.loc[(' Western Macedonia, Greece'),:]) ##sos thelei keno prin to label
+        '''
+        for item in regions_labels_list:
+            print("\n")
+            print(item)
+            print(region_df.loc[(item),['Total','Higher Education','Middle Education','Lower Education']])
+            print("Facebook Page Correlation:\n")
+            #higher = regions[item]
+            #print(higher)
+            print("\n")
+        '''
 
 def main():
+
+    regions = pd.read_excel('excels/lite/RegionDF.xlsx')
+    
+
     education = pd.read_excel('excels/elstat/formated epipedo ekpaideusis.xlsx')
-    print_dataframes_excels(education)
+    print_dataframes_education(regions,education)
 
 
 
