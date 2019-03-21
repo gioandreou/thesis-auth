6a660e951f718fea6f75d2958e501b57b9403c0a
giorgos-andreou <gioandreou1995@gmail.com>
2019-03-21 17:47:26 +0200

    nn

--- running files/tete.py
+++ running files/tete.py
@@ -7,3 +7,14 @@ import pycountry
 import pandas as pd
 import plotly.plotly as py
 import plotly.graph_objs as go
+
+
+app = dash.Dash()
+
+df_occupation = pd.read_excel('excels/elstat/formated katastasi asxolias regions_.xlsx')
+
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    #print(df_family)
+    print(df_occupation.columns)
+    #ll = df_family[df_family['Region']=='Greece']['Total'].item()
+    #print(ll)
